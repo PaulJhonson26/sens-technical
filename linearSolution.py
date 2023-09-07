addd1494ce282482b7b70e4c068d7b6232e9c652
Eliott Regnier <eliott2610@gmail.com>
2023-09-07 10:18:11 +0200

    adding readability

--- linearSolution.py
+++ linearSolution.py
@@ -13,30 +13,35 @@ top_100_films = pd.read_csv('top_100_films.csv')
 
 top_with_stats = pd.DataFrame()
 
+# getting the columns for each top 100 movie to be able to train on them
 for i in range(100):
         top100_row = top_100_films.loc[i]
         top100_id = top100_row['id']
         row_with_stats = sc_films[sc_films['id'] == top100_id]
         top_with_stats = top_with_stats._append(row_with_stats, ignore_index = True)
 
+# getting independent and dependent parameters
 top_indeps = top_with_stats.iloc[:, 2:17]
 
 top_indeps["date_release"] = top_indeps["date_release"].str[:4].fillna(2000).astype(int)-2000
 sc_indeps = sc_films.iloc[:, 2:17]
 sc_indeps["date_release"] = sc_indeps["date_release"].str[:4].fillna(2000).astype(int)-2000
 
+# splitting top_100 into training and testing data sets
 X_train, X_test, Y_train, Y_test = train_test_split(top_indeps[["date_release", 'rating', 'wish_list_count', 'review_count', 'list_count', 'n_1', 'n_2', 'n_3', 'n_4', 'n_5', 'n_6', 'n_7', 'n_8', 'n_9', 'n_10']]
 , top_100_films["rank"], test_size=0.1, random_state=26)
 
 # Lasso best alpha = 2090
+# Using alpha regularization to see if any parameters don't contribute to a better solution
 lasso_model = Lasso(alpha=2090)
 lasso_model.fit(X_train, Y_train)
 
-
+# calculating mean squared error for model performance
 lasso_predictions = lasso_model.predict(X_test)
 lasso_mse = mean_squared_error(Y_test, lasso_predictions)
 print("Lasso Mean Squared Error: {lasso_mse}")
 
+#predicting the rank of all movies in our databse
 pred = lasso_model.predict(sc_indeps)
 print(pred)
 print(lasso_model.coef_)
@@ -51,11 +56,13 @@ print(lasso_model.coef_)
 # print(sc_indeps)
 
 sc_films_ranked = sc_films
+#adding prediction as a column and sorting by those values
 sc_films_ranked["rating_count"] = pred
 
 sc_films_ranked = sc_films_ranked.sort_values(by='rating_count', ascending=True)
 sc_films_ranked.reset_index(drop=True, inplace=True)
 
+#putting into top_200 format
 def finalize(sc_films_ranked):
     final_frame = pd.DataFrame(columns=['rank', 'id', 'title', 'rating_count'])
     for i in range(200):
@@ -63,17 +70,18 @@ def finalize(sc_films_ranked):
     final_frame["rank"] = range(1, 201)
     return final_frame
 
-
+# testing our solution
 def print_test_answer(solution):
     score = 0
     for i in range(0, 100):
         top100_row = top_100_films.loc[i]
         top100_id = top100_row['id']
+        #find corresponding movie from top 100 in our solution
         myrank = solution[solution['id'] == top100_id]
         myrank_number = myrank.index[0]
 
         print("I ranked it: ", myrank_number, "it should be: ", i)
-
+        # giving score based on absolute difference in position
         score += abs(i - myrank_number)
 
     print("predictions are off by: ",score/100 , " on average")
--- solution.py
+++ solution.py
@@ -10,34 +10,36 @@ import numpy as np
 sc_films = pd.read_csv('sc_films.csv')
 top_100_films = pd.read_csv('top_100_films.csv')
 
+#method sorts a dataframe by applying weights to each attribute, calculating a score and sorting based on that score
 def sort(best_solution, s_w):
 
-    for i in range(2780):
+    for i in range(2780): #for every row in the dataframe
         current_row = best_solution.loc[i]
         score = 0
-        for j in range(15):
-            if j == 0:
-                if type(current_row[j+2]) is not float:
-                    score += s_w[j]*(int(current_row[j+2][:4])-2015)
+        for j in range(15):# apply each weight to each column
+            if j == 0:# converting year to int
+                if type(current_row[j+2]) is not float:#some years aren't registered so ignoring those
+                    score += s_w[j]*(int(current_row[j+2][:4])-2015) #multiply weight by year (-2015 is to standardize)
             else:
                     score += s_w[j]*current_row[j+2]
 
-        best_solution.loc[i, "rating_count"] = score
-    best_solution = best_solution.sort_values(by='rating_count', ascending=False)
-    best_solution.reset_index(drop=True, inplace=True)
+        best_solution.loc[i, "rating_count"] = score #adding score column to dataframe
+    best_solution = best_solution.sort_values(by='rating_count', ascending=False) #sorting by score
+    best_solution.reset_index(drop=True, inplace=True) #reset index to new sorted order
     return best_solution
 
+#method compares the rank of top 100 movies against our current solution
 def test_answer(solution):
     score = 0
-    for i in range(0, 99):
+    for i in range(0, 99): #for every row in top 100 movie
         top100_row = top_100_films.loc[i]
-        top100_id = top100_row['id']
+        top100_id = top100_row['id'] #find its id and compare corresponding id in solution
         myrank = solution[solution['id'] == top100_id]
         myrank_number = myrank.index[0]
-        score += abs(i - myrank_number)
+        score += abs(i - myrank_number) #calculate score based on absolute difference in position
     return score
 
-
+#same method as above but for displaying final results
 def print_test_answer(solution):
     score = 0
     for i in range(100):
@@ -55,24 +57,30 @@ def print_test_answer(solution):
             print("No match found for top100_id:", top100_id)
             score += 200
     print("predictions are off by: ",score/100 , " on average")
+
+#initialize start of algorithm
 solution = sc_films
 solution["rating_count"] = 0
 best_score = float('inf')
 best_solution = solution
+#s_w is the weights associated to each column in the dataframe (excluding id and title which should have no impact)
 #s_w = [1.4494125133328308, 3.447909116110372, 2.5058525699837486, -1.4835631120411323, 0.5231162644731713, -0.49223891309495027, 0.3019758134820161, 1.5479072196041235, -1.270644498999873, -1.39576703053954, 0.5829263854314388, 0.6525957825103781, -0.27451243334954856, -0.13131296805104342, 5.750111650709516]# 500 its, score: 1954
 s_w = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 best_sw = copy.deepcopy(s_w)
-score_evolution = []
+score_evolution = [] # arrays to keep track of score changes over time to see how it trends
 best_score_evolution = []
-for i in range(100):
+#number of iterations can change but I found that 500 is very sufficient as solution plateus around 500
+for i in range(500):
     randIdx = random.randint(0, 14)
     randDelt = random.uniform(-1,1)
-    s_w[randIdx] += randDelt
-    solution = sort(best_solution, s_w)
-    current_score = test_answer(solution)
+    s_w[randIdx] += randDelt #randomly change one of the 15 weights by an amount between -1 and 1
+    solution = sort(best_solution, s_w) #sort our current solution based on these new weights
+    current_score = test_answer(solution) #calculate the score of our sorted solution
     print(i," - final score: ", current_score)
+    #keeping track of score progression
     if current_score <= 15000:
         score_evolution.append(current_score)
+    # update best solution weights and score if a better solution is found
     if (current_score < best_score):
         best_score_evolution.append(current_score)
         best_solution = solution
@@ -82,6 +90,7 @@ for i in range(100):
         best_score_evolution.append(best_score)
         s_w = copy.deepcopy(best_sw)
 
+# converting solution into top_200 format
 def finalize(best_solution):
     final_frame = pd.DataFrame(columns=['rank', 'id', 'title', 'rating_count'])
     for i in range(200):
@@ -116,4 +125,3 @@ print("best weights: ", best_sw)
 print("best_score: ", best_score)
 print("best_solution: ", best_solution)
 print_test_answer(best_solution)
-
